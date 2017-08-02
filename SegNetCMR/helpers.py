6e6aa02907b3d156174cfe1a5f8e9c274c080778
Matthew Shun-Shin <m@shun-shin.com>
2017-08-02 12:18:01 +0100

    Add output with images mixed with binary version of output labels

--- SegNetCMR/helpers.py
+++ SegNetCMR/helpers.py
@@ -15,5 +15,9 @@ def add_output_images(images, logits, labels):
     output_image_binary = tf.cast(output_image_binary[...,None], tf.float32) * 128/255
     tf.summary.image('output_labels', output_image_binary, max_outputs=3)
 
+    output_labels_mixed_r = output_image_binary[...,0] + tf.multiply(images[...,0], (1-output_image_binary[...,0]))
+    output_labels_mixed = tf.stack([output_labels_mixed_r, output_image_gb, output_image_gb], axis=3)
+    tf.summary.image('output_labels_mixed', output_labels_mixed, max_outputs=3)
+
     return
 
